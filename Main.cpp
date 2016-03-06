21f38374aef88faee2035d4b5226d6e2e56a9db9
Greiner <soren@greiner.dk>
2016-03-06 22:02:14 +0100

    Added seed for rand function.

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,13 @@
 #include <algorithm>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 
 int main(int argc, char* argv[])
 {
 	printf("Hello Mathematics\n");
+
+	srand(time(NULL));
 
 	size_t maxDigits = 4;
 	uint8_t* pLeftDigits = new uint8_t[maxDigits];
@@ -14,18 +17,36 @@ int main(int argc, char* argv[])
 	memset(pRightDigits, 0, maxDigits);
 
 	printf(" ");
+	bool bLeading = true;
 	for(size_t i = 0; i < maxDigits; i++)
 	{
 		pLeftDigits[i] = rand() % 9;
-		printf("%1d", pLeftDigits[i]);
+		if(bLeading && (pLeftDigits[i] == 0))
+		{
+			printf(" ");
+		}
+		else
+		{
+			bLeading = false;
+			printf("%1d", pLeftDigits[i]);
+		}
 	}
 	printf("\n");
 
 	printf("-");
+	bLeading = true;
 	for(size_t i = 0; i < maxDigits; i++)
 	{
 		pRightDigits[i] = rand() % 9;
-		printf("%1d", pRightDigits[i]);
+		if(bLeading && (pRightDigits[i] == 0))
+		{
+			printf(" ");
+		}
+		else
+		{
+			bLeading = false;
+			printf("%1d", pRightDigits[i]);
+		}
 	}
 	printf("\n");
 
